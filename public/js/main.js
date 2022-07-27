3f5deba454814aadc13a1bb21a40bf6fad0a631f
f4zza <lazie.piscean@live.co.uk>
2022-07-27 13:21:51 +0530

    adding additinal features

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,18 @@
 const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
+// const thumbDown = document.querySelectorAll('.fa-thumbs-down')
 
-Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click', deleteQuote)
+Array.from(deleteText).forEach((e)=>{
+    e.addEventListener('click', deleteQuote)
 })
 
-Array.from(thumbText).forEach((element)=>{
-    element.addEventListener('click', addLike)
+Array.from(thumbText).forEach((e)=>{
+    e.addEventListener('click', addLike)
 })
+
+// Array.from(thumbDown).forEach((e)=> {
+//     e.addEventListener('click', removeLike)
+// })
 
 async function deleteQuote(){
     const aQuote = this.parentNode.childNodes[1].innerText
@@ -33,7 +38,7 @@ async function deleteQuote(){
 async function addLike(){
     const aQuote = this.parentNode.childNodes[1].innerText
     const eQuote = this.parentNode.childNodes[3].innerText
-    const toLikes = Number(this.parentNode.childNodes[5].innerText)
+    const toLikes = Number(this.parentNode.childNodes[7].innerText)
     try{
         const res = await fetch('addOneLike', {
             method: 'put',
@@ -52,3 +57,26 @@ async function addLike(){
         console.log(err)
     }
 }
+
+// async function removeLike(){
+//     const aQuote = this.parentNode.childNodes[1].innerText
+//     const eQuote = this.parentNode.childNodes[3].innerText
+//     const toLikes = Number(this.parentNode.childNodes[7].innerText)
+//     try{
+//         const res = await fetch('removeOneLike', {
+//             method: 'put',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//               'arQuoteL': aQuote,
+//               'enQuoteL': eQuote,
+//               'likesL': toLikes
+//             })
+//           })
+//         const data = await res.json()
+//         console.log(data)
+//         location.reload()
+
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
